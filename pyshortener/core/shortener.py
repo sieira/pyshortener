b92dadd63a44762b782c776c6de293df543fe2f0
sieira <luis.sieira@spacebar.fr>
2024-09-19 21:51:43 +0200

    Add expiration and click count

--- pyshortener/core/shortener.py
+++ pyshortener/core/shortener.py
@@ -2,13 +2,16 @@ from django.db import connection
 
 import string
 import random
+import time
 
 from core.models.url import Url
 
 
 class Shortener:
     CHARSET = string.ascii_letters + string.digits
+    # TODO move to settings
     SHORT_LEN = 6
+    EXPIRATION_SECONDS = 365 * 24 * 60 * 60
 
     @staticmethod
     def __get_from_long(long_url: str) -> Url | None:
@@ -20,16 +23,30 @@ class Shortener:
     @staticmethod
     def __create(long_url: str) -> Url:
         short_url = Shortener.__shorten_url()
+        expiration_timestamp = int(time.time()) + Shortener.EXPIRATION_SECONDS
         with connection.cursor() as cursor:
-            cursor.execute(f'INSERT INTO url (long_url, short_url) VALUES ("{long_url}", "{short_url}")')
+            cursor.execute(
+                f'INSERT INTO url (long_url, short_url, expiration_date) VALUES ("{long_url}", "{short_url}", {expiration_timestamp})'
+            )
         return Url(long_url, short_url)
 
     @staticmethod
-    def get_from_short(short_url: str) -> Url:
+    def __shorten_url() -> str:
+        return ''.join(random.choice(Shortener.CHARSET) for _ in range(Shortener.SHORT_LEN))
+
+    @staticmethod
+    def __increment_count(short_url: str) -> None:
+        with connection.cursor() as cursor:
+            cursor.execute(f'UPDATE url SET click_count = click_count + 1 WHERE short_url="{short_url}"')
+
+    @staticmethod
+    def get_from_short(short_url: str) -> Url | None:
         with connection.cursor() as cursor:
             cursor.execute(f'SELECT long_url, short_url FROM url WHERE short_url="{short_url}"')
             record = cursor.fetchone()
-        return None if record is None else Url(record[0], record[1])
+        if record:
+            Shortener.__increment_count(short_url)
+            return Url(record[0], record[1])
 
     @staticmethod
     def get_or_create(long_url: str) -> Url:
@@ -37,7 +54,3 @@ class Shortener:
         if record is None:
             record = Shortener.__create(long_url)
         return record
-
-    @staticmethod
-    def __shorten_url() -> str:
-        return ''.join(random.choice(Shortener.CHARSET) for _ in range(Shortener.SHORT_LEN))
--- pyshortener/create_database.sql
+++ pyshortener/create_database.sql
@@ -1,5 +1,17 @@
 CREATE TABLE url (
     ID int NOT NULL UNIQUE AUTO_INCREMENT,
     long_url VARCHAR(2000) NOT NULL UNIQUE,
-    short_url VARCHAR(6) NOT NULL UNIQUE
-)
+    short_url VARCHAR(6) NOT NULL UNIQUE,
+    click_count INT DEFAULT 0,
+    expiration_date INT
+);
+
+CREATE EVENT IF NOT EXISTS
+  ClearExpiredUrls
+ON SCHEDULE EVERY 1 DAY
+DO
+BEGIN
+DELETE FROM
+  url
+WHERE expiration_date < NOW();
+END
