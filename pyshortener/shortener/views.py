2583007c322e482fd2144bc7da52e0abea64fab5
sieira <luis.sieira@spacebar.fr>
2024-09-20 11:48:03 +0200

    validate input url

--- pyshortener/shortener/views.py
+++ pyshortener/shortener/views.py
@@ -1,5 +1,8 @@
 import logging
 
+from django.core.exceptions import ValidationError
+from django.core.validators import URLValidator
+
 from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
@@ -11,7 +14,14 @@ LOGGER = logging.getLogger(__name__)
 
 @api_view(['POST'])
 def shorten(request):
-    url = request.data['url']
+    url = request.data.get('url', '')
+    validator = URLValidator()
+
+    try:
+        validator(url)
+    except ValidationError:
+        return Response(status=status.HTTP_400_BAD_REQUEST)
+
     shortened_url = Shortener.get_or_create(url).short_url
     # TODO get it form settings or request
     own_host = 'localhost:8080'
